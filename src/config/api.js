d279a3faddb2af411e5c4eaba0c7b7553facff32
PremsagarChakrapani <premsagarc11@gmail.com>
2024-08-09 22:03:56 +0530

    pushing code?

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 const API_DOMAIN = "https://gnews.io/api/v4/top-headlines?country=";
 const API_SEARCH_DOMAIN = "https://gnews.io/api/v4/search?q=";
-const API_KEY = process.env.REACT_APP_API_KEY;;
+const API_KEY = "7ec6cce4642432acfe173c08ceb504f0";;
 export const endpointPath = (country, category) =>
   `${API_DOMAIN}${country}&lang=en&category=${category}&apikey=${API_KEY}`;
 export const endpointSearch = (searchQuery) =>
